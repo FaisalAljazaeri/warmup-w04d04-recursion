4b8624675ce4ed910b5e2782d1bcd87cac6a621d
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-02-12 10:02:30 +0300

    Solve 1 and 2

--- recursion.js
+++ recursion.js
@@ -1,22 +1,44 @@
 // Write code inside the functions
 // You will have to figure out what parameters to include
 // All functions must use recursion
-
-function findMax(){
+function findMax(arr, i, max = -Infinity) {
     // This function returns the largest number in a given array.
-}
+    if (i === arr.length) {
+        return max;
+    }
 
-function factorial(){
+    if (arr[i] > max) {
+        return findMax(arr, i + 1, arr[i]);
+    } else {
+        return findMax(arr, i + 1, max);
+    }
+}
+findMax([1, 44, 3, 11, 20, 100, 19, 39, -1, -3], 0);
+
+function factorial(n) {
     // This function returns the factorial of a given number.
-}
+    if (n === 0) {
+        return 1;
+    }
 
-function fibonacci(){
+    return n * factorial(n - 1);
+}
+factorial(5);
+
+function fibonacci(n, i = 1, t = 0) {
     // This function returns the Nth number in the fibonacci sequence.
     // https://en.wikipedia.org/wiki/Fibonacci_number
     // For this function, the first two fibonacci numbers are 1 and 1
-}
 
-function coinFlips(){
+    if (t === n) {
+        return 1;
+    }
+
+    return i + fibonacci(i + 1, t = t + 1);
+}
+fibonacci(4); // 1,1,2,3,5,8,13
+
+function coinFlips() {
     // This function returns an array of all possible outcomes from flipping a coin N times.
     // Input type: Integer
     // For example, coinFlips(2) would return the following:
@@ -25,7 +47,7 @@ function coinFlips(){
     // Represent the two outcomes of each flip as "H" or "T"
 }
 
-function letterCombinations(){
+function letterCombinations() {
     // This function returns an array of all combinations of the given letters
     // Input type: Array of single characters
     // For example, letterCombinations(["a","b","c"]) would return the following:
